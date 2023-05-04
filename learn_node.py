15450cffde66ef20e5caac9024ff2f39b26657b8
ourui <1447549515@qq.com>
2023-05-04 06:49:08 +0500

    reformat

--- learn_node.py
+++ learn_node.py
@@ -48,14 +48,17 @@ parser.add_argument('--n_head', type=int, default=2)
 parser.add_argument('--n_epoch', type=int, default=15, help='number of epochs')
 parser.add_argument('--n_layer', type=int, default=2)
 parser.add_argument('--lr', type=float, default=3e-4)
-parser.add_argument('--tune', action='store_true', help='parameters tunning mode, use train-test split on training data only.')
+parser.add_argument('--tune', action='store_true',
+                    help='parameters tunning mode, use train-test split on training data only.')
 parser.add_argument('--drop_out', type=float, default=0.1, help='dropout probability')
 parser.add_argument('--gpu', type=int, default=0, help='idx for the gpu to use')
 parser.add_argument('--node_dim', type=int, default=None, help='Dimentions of the node embedding')
 parser.add_argument('--time_dim', type=int, default=None, help='Dimentions of the time embedding')
-parser.add_argument('--agg_method', type=str, choices=['attn', 'lstm', 'mean'], help='local aggregation method', default='attn')
+parser.add_argument('--agg_method', type=str, choices=['attn', 'lstm', 'mean'], help='local aggregation method',
+                    default='attn')
 parser.add_argument('--attn_mode', type=str, choices=['prod', 'map'], default='prod')
-parser.add_argument('--time', type=str, choices=['time', 'pos', 'empty'], help='how to use time information', default='time')
+parser.add_argument('--time', type=str, choices=['time', 'pos', 'empty'], help='how to use time information',
+                    default='time')
 
 parser.add_argument('--new_node', action='store_true', help='model new node')
 parser.add_argument('--uniform', action='store_true', help='take uniform sampling from temporal neighbors')
@@ -119,8 +122,8 @@ max_idx = max(src_l.max(), dst_l.max())
 
 total_node_set = set(np.unique(np.hstack([g_df.u.values, g_df.i.values])))
 
-valid_train_flag = (ts_l <= test_time)  
-valid_val_flag = (ts_l <= test_time) 
+valid_train_flag = (ts_l <= test_time)
+valid_val_flag = (ts_l <= test_time)
 assignment = np.random.randint(0, 10, len(valid_train_flag))
 valid_train_flag *= (assignment >= 2)
 valid_val_flag *= (assignment < 2)
@@ -141,7 +144,7 @@ if args.tune:
     test_label_l = label_l[valid_val_flag]
 else:
     logger.info('Training use all train data')
-    valid_train_flag = (ts_l <= test_time)  
+    valid_train_flag = (ts_l <= test_time)
     train_src_l = src_l[valid_train_flag]
     train_dst_l = dst_l[valid_train_flag]
     train_ts_l = ts_l[valid_train_flag]
@@ -154,7 +157,6 @@ else:
     test_ts_l = ts_l[valid_test_flag]
     test_e_idx_l = e_idx_l[valid_test_flag]
     test_label_l = label_l[valid_test_flag]
-
 
 ### Initialize the data structure for graph and edge sampling
 adj_list = [[] for _ in range(max_idx + 1)]
@@ -179,13 +181,12 @@ tgan = TGAN(train_ngh_finder, n_feat, e_feat,
 # criterion = torch.nn.BCELoss()
 tgan = tgan.to(device)
 
-
 num_instance = len(train_src_l)
 num_batch = math.ceil(num_instance / BATCH_SIZE)
 logger.debug('num of training instances: {}'.format(num_instance))
 logger.debug('num of batches per epoch: {}'.format(num_batch))
 idx_list = np.arange(num_instance)
-np.random.shuffle(idx_list) 
+np.random.shuffle(idx_list)
 
 logger.info('loading saved TGAN model')
 model_path = f'./saved_models/{args.prefix}-{args.agg_method}-{args.attn_mode}-{DATA}.pth'
@@ -202,6 +203,7 @@ idx_list = np.arange(len(train_src_l))
 lr_criterion = torch.nn.BCELoss()
 lr_criterion_eval = torch.nn.BCELoss()
 
+
 def eval_epoch(src_l, dst_l, ts_l, label_l, batch_size, lr_model, tgan, num_layer=NODE_LAYER):
     pred_prob = np.zeros(len(src_l))
     loss = 0
@@ -210,7 +212,7 @@ def eval_epoch(src_l, dst_l, ts_l, label_l, batch_size, lr_model, tgan, num_laye
     with torch.no_grad():
         lr_model.eval()
         tgan.eval()
-        for k in range(num_batch):          
+        for k in range(num_batch):
             s_idx = k * batch_size
             e_idx = min(num_instance - 1, s_idx + batch_size)
             src_l_cut = src_l[s_idx:e_idx]
@@ -218,7 +220,7 @@ def eval_epoch(src_l, dst_l, ts_l, label_l, batch_size, lr_model, tgan, num_laye
             ts_l_cut = ts_l[s_idx:e_idx]
             label_l_cut = label_l[s_idx:e_idx]
             size = len(src_l_cut)
-            src_embed = tgan.tem_conv(src_l_cut, ts_l_cut, num_layer)            
+            src_embed = tgan.tem_conv(src_l_cut, ts_l_cut, num_layer)
             src_label = torch.from_numpy(label_l_cut).float().to(device)
             lr_prob = lr_model(src_embed).sigmoid()
             loss += lr_criterion_eval(lr_prob, src_label).item()
@@ -228,13 +230,12 @@ def eval_epoch(src_l, dst_l, ts_l, label_l, batch_size, lr_model, tgan, num_laye
     return auc_roc, loss / num_instance
 
 
-
 for epoch in tqdm(range(args.n_epoch)):
     lr_pred_prob = np.zeros(len(train_src_l))
     np.random.shuffle(idx_list)
     tgan = tgan.eval()
     lr_model = lr_model.train()
-    #num_batch
+    # num_batch
     for k in range(num_batch):
         s_idx = k * BATCH_SIZE
         e_idx = min(num_instance - 1, s_idx + BATCH_SIZE)
@@ -242,13 +243,13 @@ for epoch in tqdm(range(args.n_epoch)):
         dst_l_cut = train_dst_l[s_idx:e_idx]
         ts_l_cut = train_ts_l[s_idx:e_idx]
         label_l_cut = train_label_l[s_idx:e_idx]
-        
+
         size = len(src_l_cut)
-        
+
         lr_optimizer.zero_grad()
         with torch.no_grad():
             src_embed = tgan.tem_conv(src_l_cut, ts_l_cut, NODE_LAYER)
-        
+
         src_label = torch.from_numpy(label_l_cut).float().to(device)
         lr_prob = lr_model(src_embed).sigmoid()
         lr_loss = lr_criterion(lr_prob, src_label)
@@ -257,18 +258,9 @@ for epoch in tqdm(range(args.n_epoch)):
 
     train_auc, train_loss = eval_epoch(train_src_l, train_dst_l, train_ts_l, train_label_l, BATCH_SIZE, lr_model, tgan)
     test_auc, test_loss = eval_epoch(test_src_l, test_dst_l, test_ts_l, test_label_l, BATCH_SIZE, lr_model, tgan)
-    #torch.save(lr_model.state_dict(), './saved_models/edge_{}_wkiki_node_class.pth'.format(DATA))
+    # torch.save(lr_model.state_dict(), './saved_models/edge_{}_wkiki_node_class.pth'.format(DATA))
     logger.info(f'train auc: {train_auc}, test auc: {test_auc}')
 
 test_auc, test_loss = eval_epoch(test_src_l, test_dst_l, test_ts_l, test_label_l, BATCH_SIZE, lr_model, tgan)
-#torch.save(lr_model.state_dict(), './saved_models/edge_{}_wkiki_node_class.pth'.format(DATA))
+# torch.save(lr_model.state_dict(), './saved_models/edge_{}_wkiki_node_class.pth'.format(DATA))
 logger.info(f'test auc: {test_auc}')
-
-
-
-
- 
-
-
-
-
